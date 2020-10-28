48edf6d698e34480501c89d857f3b87629287fd8
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2020-10-28 15:10:49 -0500

    Added Menu Functionality
    Added main menu and pause system (same system as Singleton), which can be extended once more of the game is made.

---
+++ CarpoolingProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,95 @@
+﻿/*
+ * Benjamin Schuster
+ * Carpool Sim [Project 4+]
+ * Manages menu setings
+ */
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    public int score;
+    public GameObject pauseMenu;
+    public bool isPaused = false;
+
+    private string CurrentLevelName = "MainMenu";
+
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            //Make sure game manager is global
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            Debug.LogError("Trying to instantiate second singleton");
+        }
+    }
+    
+    private void Update()
+    {
+        //pauses and resumes on esc press. Cannot pause on main menu screen
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused && !CurrentLevelName.Equals("MainMenu"))
+        {
+            Pause();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused && !CurrentLevelName.Equals("MainMenu"))
+        {
+            UnPause();
+        }
+    }
+
+    //load and unload levels
+    public void LoadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+        if (ao == null)
+        {
+            Debug.LogError("[GameManager] Unable to load: " + levelName);
+            return;
+        }
+        CurrentLevelName = levelName;
+    }
+
+    public void UnLoadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(levelName);
+        if (ao == null)
+        {
+            Debug.LogError("[GameManager] Unable to unload: " + levelName);
+        }
+        CurrentLevelName = "MainMenu";
+    }
+
+    public void UnloadCurrentLevel()
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(CurrentLevelName);
+        if (ao == null)
+        {
+            Debug.LogError("[GameManager] Unable to unload: " + CurrentLevelName);
+        }
+        CurrentLevelName = "MainMenu";
+    }
+
+    //pausing and unpausing
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        isPaused = true;
+    }
+    public void UnPause()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        isPaused = false;
+    }
+
+}
