a26385d620d7d3a0c81a662fd96374bb46a6b05e
BenSchu438 <70485582+BenSchu438@users.noreply.github.com>
2020-11-11 16:41:18 -0600

    Updated Tutorial
    Divided text, slightly modified megamap. Removed megamap from tutorial, might disable for playtesting.

--- CarpoolingProject/Assets/Scripts/MapSwap.cs
+++ CarpoolingProject/Assets/Scripts/MapSwap.cs
@@ -30,12 +30,14 @@ public class MapSwap : MonoBehaviour
             minimap.SetActive(false);
             megamap.SetActive(true);
             minimapUsed = false;
+            Time.timeScale = 0f;
         }
         else if (Input.GetKeyDown(KeyCode.M) && !minimapUsed)
         {
             minimap.SetActive(true);
             megamap.SetActive(false);
             minimapUsed = true;
+            Time.timeScale = 1f;
         }
 
     }
