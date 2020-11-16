cdd67f891b6486832c66f4c34558fca96fd31182
Jerod Lockhart <60198376+almosthree@users.noreply.github.com>
2020-11-16 02:47:34 -0600

    Added breaking, added turning

--- CarpoolingProject/Assets/Scripts/ControlPlayer.cs
+++ CarpoolingProject/Assets/Scripts/ControlPlayer.cs
@@ -1,17 +1,23 @@
-﻿    using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ControlPlayer : MonoBehaviour
 {
-    public float speed = 10;
     public float fuelEfficiency = 1f;
-
-    private float turnSpeed = 50;
-    private float horizontalInput;
-    private float forwardInput;
-    private FuelBar gasCheck;
+    public float horizontalInput;
+    public bool brakeInput;
+    public float steerAngle;
+    public float forwardInput;
     private bool gameOver = false;
+
+    public WheelCollider frontDriverW = new WheelCollider(), frontPassW = new WheelCollider();
+    public WheelCollider rearDriverW = new WheelCollider(), rearPassW = new WheelCollider();
+    public Transform frontDriverT, frontPassT;
+    public Transform rearDriverT, rearPassT;
+    public float maxSteerAngle = 30;
+    public float motorForce = 700;
+    public float brake = 500000;
 
     // Start is called before the first frame update
     void Start()
@@ -20,28 +26,84 @@ public class ControlPlayer : MonoBehaviour
         GameManager.usedFuel = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetInput()
     {
-        //speed= FuelBar.isFuelEmpty(speed);
-        gameOver = FuelBar.isFuelEmpty();
-
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-
-        //can only turn if moving forward or backward
-        if (forwardInput != 0)
-        {
-            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
-        }
-
-        //using fuel while moving
+        brakeInput = Input.GetKey("space");
         if (forwardInput == 1 || forwardInput == -1)
         {
             GameManager.usedFuel += Time.deltaTime * fuelEfficiency;
         }
+
+    }
+
+    private void Steer()
+    {
+        steerAngle = maxSteerAngle * horizontalInput;
+
+        if (!brakeInput)
+        {
+            frontDriverW.steerAngle = steerAngle;
+            frontPassW.steerAngle = steerAngle;
+
+        }
+        else
+        {
+            frontDriverW.steerAngle = -steerAngle;
+            frontPassW.steerAngle = -steerAngle;
+        }
+    }
+
+    private void Accelerate()
+    {
+
+        if(brakeInput)
+        {
+            frontDriverW.brakeTorque = brake;
+            frontPassW.brakeTorque = brake;
+
+        }
+        else
+        {
+            Debug.Log(frontDriverW.motorTorque);
+ 
+            frontDriverW.motorTorque = forwardInput * motorForce;
+            frontPassW.motorTorque = forwardInput * motorForce;
+            frontDriverW.brakeTorque = 0;
+            frontPassW.brakeTorque = 0;
+        }
+    }
+
+    private void UpdateWheelPoses()
+    {
+        UpdateWheelPose(frontDriverW, frontDriverT);
+        UpdateWheelPose(rearDriverW, rearDriverT);
+        UpdateWheelPose(frontPassW, frontPassT);
+        UpdateWheelPose(rearPassW, rearPassT);
+
+    }
+
+    private void UpdateWheelPose(WheelCollider collider, Transform transform)
+    {
+        Vector3 pos = transform.position;
+        Quaternion quat = transform.rotation;
+
+        collider.GetWorldPose(out pos, out quat);
+
+        transform.position = pos;
+        transform.rotation = quat;
+    }
+
+    void FixedUpdate()
+    {
+        GetInput();
+        Steer();
+        Accelerate();
+        UpdateWheelPoses();
+        gameOver = FuelBar.isFuelEmpty();
+        if (frontDriverW.rpm > 500)
+            frontDriverW.motorTorque = 0;
     }
 
 }
