22034fecced28a5fb308119a6d16f6aaad8c95e5
LoganRossBradley <60478655+LoganRossBradley@users.noreply.github.com>
2020-12-10 18:39:37 -0600

    Revert "Fixed spawns level 1 added random roadblocks"
    This reverts commit 187de8a7742d00987f469c92ec28f37c95cf5782.

--- CarpoolingProject/Assets/Scripts/SpawnManager.cs
+++ CarpoolingProject/Assets/Scripts/SpawnManager.cs
@@ -7,13 +7,12 @@ using UnityEngine.SceneManagement;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] roadblocks = new GameObject[3];
+
     public GameObject[] characters = new GameObject[5];
-    public Vector3[] characterLocations = new Vector3[10];
+    public Vector3[] spawnLocations = new Vector3[10];
 
     public int randChar;
     public int randLocation;
-    public int randRoadblock;
 
     // Start is called before the first frame update
     void Start()
@@ -21,36 +20,13 @@ public class SpawnManager : MonoBehaviour
         //set scene this object is in to active scene so objects properly spawn in it instead of main menu scene
         SceneManager.SetActiveScene(SceneManager.GetSceneByName(GameManager.CurrentLevelName));
 
-        //max 12
         spawnCharacter(5);
-        //max 10
-        spawnParkedCars(5);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    public void spawnParkedCars(int spawnAmount)
-    {
-        int[] carsUsed = new int[spawnAmount];
-
-        //create characters to pick up in random pos equal to ammount passed in
-        for (int i = 0; i < spawnAmount; i += 0)
-        {
-            randRoadblock = UnityEngine.Random.Range(0, roadblocks.Length - 1);
-            //checks to see if random location has already been used
-            if (!carsUsed.Contains(randRoadblock))
-            {
-                //randChar = UnityEngine.Random.Range(0, characters.Length - 1);
-                //Instantiate(characters[randChar], CharacterLocations[randRoadblock], characters[randChar].transform.rotation);
-                roadblocks[randRoadblock].SetActive(true);
-                carsUsed[i] = randRoadblock;
-                i++;
-            }
-        }
     }
 
     public void spawnCharacter(int spawnAmount)
@@ -60,12 +36,12 @@ public class SpawnManager : MonoBehaviour
         //create characters to pick up in random pos equal to ammount passed in
         for(int i = 0; i < spawnAmount; i +=0)
         {
-            randLocation = UnityEngine.Random.Range(0, characterLocations.Length - 1);
+            randLocation = UnityEngine.Random.Range(0, spawnLocations.Length - 1);
             //checks to see if random location has already been used
             if(!locationsUsed.Contains(randLocation))
             {
                 randChar = UnityEngine.Random.Range(0, characters.Length - 1);
-                Instantiate(characters[randChar], characterLocations[randLocation], characters[randChar].transform.rotation);
+                Instantiate(characters[randChar], spawnLocations[randLocation], characters[randChar].transform.rotation);
                 locationsUsed[i] = randLocation;
                 i++;
             }
